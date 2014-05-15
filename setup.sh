5a1117c0bd8f60b9f2e5c4701521cf3bcdabe805
M.X. Grey <mxgrey@gatech.edu>
2014-05-14 19:05:27 -0500

    uninstall added to setup script

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ ShowUsage()
     echo " This is for installing HuboCan on the physical Hubo itself."
     echo " When running the script, pass in 'robot' followed by the"
     echo " version of Hubo which you have. Ex:"
-    echo "$ ./setup robot Hubo2Plus"
+    echo "   $ ./setup robot Hubo2Plus"
     echo " "
     echo " Currently supported versions include:"
     echo " - Hubo2Plus"
@@ -19,7 +19,7 @@ ShowUsage()
     echo " (i.e. a computer which is for developing code or for"
     echo " an operator to use). When running the script, simply"
     echo " pass in 'workstation'. Ex:"
-    echo "$ ./setup workstation"
+    echo "   $ ./setup workstation"
     echo " "
 }
 
@@ -28,9 +28,11 @@ MakeBuildDirectory()
     if [ -d "build" ]
     then
         echo 'Build directory was found'
+        new_build_dir=0
     else
         echo 'Making build directory'
         mkdir build
+        new_build_dir=1
     fi
 }
 
@@ -98,6 +100,8 @@ RawRobotInstall()
     AutostartManager
 
     /usr/bin/hubocan_setup_default_config "$2"
+
+    echo 'robot' > /opt/hubo/install_type
 }
 
 RobotInstall()
@@ -140,6 +144,44 @@ WorkstationInstall()
 
     CreateOptHubo
     CopyDevices
+
+    echo 'workstation' > /opt/hubo/install_type
+}
+
+HuboUninstall()
+{
+    MakeBuildDirectory
+
+    cd build
+
+    if [ -e "/opt/hubo/install_type" ]
+    then
+        install_type=$(sudo cat /opt/hubo/install_type)
+    else
+        install_type=workstation
+    fi
+
+    if [ "$new_build_dir" -eq 1 ]
+    then
+        echo 'Since a build directory did not exist, we must compile and install before uninstalling'
+        if [ "$install_type" = 'robot' ]
+        then
+            cmake .. -DBuildHuboQt=OFF -DCMAKE_INSTALL_PREFIX=/usr
+        else
+            cmake .. -DBuildHuboQt=ON  -DCMAKE_INSTALL_PREFIX=/usr
+        fi
+
+        make
+        sudo make install
+    fi
+
+    sudo make uninstall
+
+    if [ "$install_type" = 'robot' ]
+    then
+        sudo update-rc.d -f hubo-manager remove
+        sudo rm -f /etc/init.d/hubo-manager
+    fi
 }
 
 case "$1" in
@@ -149,6 +191,10 @@ case "$1" in
 
     'workstation')
         WorkstationInstall $@
+    ;;
+
+    'uninstall')
+        HuboUninstall
     ;;
 
     *)
